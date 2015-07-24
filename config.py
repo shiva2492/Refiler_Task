53d0d3f4cc750a91112172224e27d42efdcbdf14
shiva <shiva2492@users.noreply.github.com>
2015-07-25 02:27:45 +0530

    Updating config.py
    updating config for database creation

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 from codebase import app
 
+#run the following command from terminal to create the database and schema 
+#go into the codebase directory and then run the command 
+# mysql -u root -p < Restaurant.sql
+#password will be prompted to enter for root user of database server
 
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='student'
 app.config['MYSQL_DATABASE_DB'] ='Restaurant'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'Your private key'
 
